e93cde19cdeb5e4e4576f2b4c11f44d7118084ed
murselsen <murselsen_35@hotmail.com>
2023-10-11 12:48:13 +0300

    11.10.2023 Commit

--- main.js
+++ main.js
@@ -6,6 +6,7 @@
 const url = require("url");
 const path = require("path");
 const fs = require("fs");
+const fsExtra = require("fs-extra")
 
 const electron = require("electron");
 const processManager = require("./process")
@@ -16,6 +17,7 @@ const {
 // User Modules
 const mainApp = require("./Apps");
 const child_process = require("child_process");
+const {request} = require("express");
 let execProcess
 
 const EtsDecrypt = () => {
@@ -25,9 +27,7 @@ const EtsDecrypt = () => {
 
         }
     });
-    childApp.emit('close', (code) => {
-        new mainApp()
-    })
+
     return childApp
 }
 
@@ -38,22 +38,25 @@ process.on("loaded", () => {
     process.stderr.write = infoLogStream.write.bind(infoLogStream);
 
 
-
-
-
     console.log("App Loaded");
 })
+process.on("disconnect", () => {
+    console.log("Process Disconnected")
+})
+
 
 // Application keeps Error log
 process.on("uncaughtException", (error) => {
     const package_json = JSON.parse(fs.readFileSync(__dirname + "/package.json", "utf-8"))
-  const errorMessage = `Uncaught Exception:\n${error.stack || error}`;
-  console.error(errorMessage)
+    const errorMessage = `Uncaught Exception:\n${error.stack || error}`;
+    console.error(errorMessage)
     fs.writeFileSync(path.join(process.env.APPDATA, package_json.name, "error.log"), errorMessage, "utf-8");
+
 });
 
 // Works when the application is ready
 app.on("ready", () => {
+    console.log(path.join(app.getPath('appData'), app.name))
     dialog.showMessageBox(null, {
         type: 'question',
         title: 'Sii Decrypt',
@@ -62,28 +65,30 @@ app.on("ready", () => {
     }).then((result) => {
         console.log("MessageBox Return Value : ", result)
         if (result.response == 1) {
-           execProcess = EtsDecrypt()
+            execProcess = EtsDecrypt()
 
+            execProcess.on('close', (code) => {
+                new mainApp()
+                const pM = new processManager()
+                pM.loaded()
+            })
         } else {
             new mainApp()
+            const pM = new processManager()
+            pM.loaded()
         }
-        const pM = new processManager()
-        pM.loaded()
+
     })
-
-  ;
-
 });
 
 
 // When all windows of the application are closed
-
-app.on("window-all-closed", () => {
-    execProcess.stdin.end();
-    app.quit();
-});
+app.on("before-quit", () => {
+    const pM = new processManager()
+    pM.killed()
+})
 
 app.on("quit", () => {
-    execProcess.stdin.end();
+
     app.quit();
 })
--- process.js
+++ process.js
@@ -6,9 +6,10 @@ const ConfigManager = require("./server/Config")
 const RadioManager = require("./server/Radio")
 const GamesConfigManager = require("./server/Games")
 const ProfileManager = require("./server/Profile")
+const fs = require("fs");
 
 class ProcessManager {
-    loaded() {
+    loaded = () => {
         const cM = new ConfigManager()
         cM.init()
         const rM = new RadioManager()
@@ -19,6 +20,13 @@ class ProcessManager {
         const pM = new ProfileManager()
         pM.init()
 
+        console.log("Process Loaded ")
+    }
+
+    killed = () => {
+        const pM = new ProfileManager()
+        pM.kill()
+        console.log("Process Killed !")
     }
 }
 
